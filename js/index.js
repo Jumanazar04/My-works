4ae0ed823074f3dd54c8bb72413ac7102cd0f95e
Jumanazar04 <yusupovjumanazar004@gmail.com>
2024-01-19 20:56:27 +0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,9 @@
+const day = document.querySelector('.day');
+const month = document.querySelector('.month');
+const year = document.querySelector('.year');
+const hour = document.querySelector('.hour');
+const minute = document.querySelector('.minute');
+const second = document.querySelector('.second');
+
+let time = Date();
+console.log(time);
