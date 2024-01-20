4247d4476ab2a5f4739b0c020a9a0809c8a97620
Jumanazar04 <yusupovjumanazar004@gmail.com>
2024-01-20 12:50:56 +0500

    again

--- js/index.js
+++ js/index.js
@@ -48,3 +48,19 @@ if(days < 10){
 }else{
     day.textContent = days;
 }
+function getTimeOne (){
+    const now = new Date()
+
+    const hours = now.getHours() < 10 ? '0' +  (now.getHours() ) : now.getHours();
+    const minutes = now.getMinutes() < 10 ? '0' +  (now.getMinutes() ) : now.getMinutes(); 
+    const seconds = now.getSeconds() < 10 ? '0' +  (now.getSeconds() ) : now.getSeconds();
+
+    hour.textContent = hours
+    minute.textContent = minutes
+    second.textContent = seconds
+    return `${hour}:${minute}`
+}
+
+setInterval(() => {
+    getTimeOne()
+}, 1000)
